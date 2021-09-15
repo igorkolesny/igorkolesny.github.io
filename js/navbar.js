a2fdd7113863b51ea4eba7e95e481617f479d969
Igor <57992358+igorkolesny@users.noreply.github.com>
2021-09-15 20:31:25 +0300

    Delete js directory

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function (event) {
-    if ($(this).scrollTop() > 700) {
-        $(".head").slideDown();
-        return false;
-    } else {
-        $(".head").slideUp();
-    }
-});
